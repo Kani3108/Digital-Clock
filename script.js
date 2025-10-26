b5dd44b80e13e8fc7da4b71ad25ee0f64551e327
Kani3108 <kanishkasinghmeena12@gmail.com>
2025-10-26 14:38:03 +0530

    Update script.js
    Signed-off-by: Kani3108 <kanishkasinghmeena12@gmail.com>

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const time=document.getElementById('time');
-const timeformat = document.getElementById('timwformat');
+const timeformat = document.getElementById('timeformat');
 
 document.addEventListener('DOMContentLoaded',()=>{
     setInterval(showTime,1000);
@@ -11,11 +11,12 @@ const showTime=()=>{
     let mins=date.getMinutes();
     let secs=date.getSeconds();
 
-    hr=hr<10 ? `0${hr}` :hr;
-    mins=mins<10 ? `0${hr}` :mins;
-    secs=secs<10 ? `0${hr}` :secs;
+    hr = hr<10 ? `0${hr}` :hr;
+    mins = mins < 10 ? `0${mins}` :mins;
+    secs = secs < 10 ? `0${secs}` :secs;
 
     time.innerHTML= `${hr} : ${mins} : ${secs}`;
     timeformat.innerHTML =hr>12? "PM" : "AM";
     //console.log("hours "+ hr + "mins "+mins + "secs "+ secs);
+
 }
